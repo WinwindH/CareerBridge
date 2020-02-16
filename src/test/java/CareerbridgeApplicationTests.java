bb9ea2e9b147e61742e554efd8d620eac8872d1f
WinwindH <993600563@qq.com>
2020-02-16 20:04:39 +0800

    CareerbridgeApplication.class

--- src/test/java/CareerbridgeApplicationTests.java
+++ src/test/java/CareerbridgeApplicationTests.java
@@ -1,9 +1,9 @@
+import edu.nju.careerbridge.CareerbridgeApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {CareerbridgeApplication.class})
 public class CareerbridgeApplicationTests {
 }
