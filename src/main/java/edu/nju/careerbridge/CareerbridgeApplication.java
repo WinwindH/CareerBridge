9f21b13057447b2d1ce4ab161d5a65ff6b4913b5
WinwindH <993600563@qq.com>
2020-02-16 19:34:15 +0800

    移动CareerBridgeApplications.java的位置

--- src/main/java/edu/nju/careerbridge/CareerbridgeApplication.java
+++ src/main/java/edu/nju/careerbridge/CareerbridgeApplication.java
@@ -1,4 +1,4 @@
-package edu.nju.careerbridge.youth;
+package edu.nju.careerbridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
