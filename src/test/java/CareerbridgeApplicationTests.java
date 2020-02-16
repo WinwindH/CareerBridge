ecf29a6c827103b8c2ef8ae273fd004f0ab46932
WinwindH <993600563@qq.com>
2020-02-16 20:10:09 +0800

    test1

--- src/test/java/CareerbridgeApplicationTests.java
+++ src/test/java/CareerbridgeApplicationTests.java
@@ -1,9 +1,18 @@
 import edu.nju.careerbridge.CareerbridgeApplication;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {CareerbridgeApplication.class})
 public class CareerbridgeApplicationTests {
+
+    @Test
+    public void test(){
+
+        assertEquals("hhh","h");
+    }
 }
